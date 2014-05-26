59132e7d0e1ec6038a60b0d2268d799b521ae544
Jochen Skulj <jochen@jochenskulj.de>
2014-05-26 19:27:12 +0200

    Added junit.jar and fixed start.sh

--- TaskManager/start.sh
+++ TaskManager/start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-export FOLIVORA_HOME=/home/joskulj/src/java/eclipse/TaskManager
+export FOLIVORA_HOME=.
 export CLASSPATH=$FOLIVORA_HOME/deploy/folivora.jar
+export CLASSPATH=$CLASSPATH:$FOLIVORA_HOME/lib/log4j-1.2.17.jar
 export MAIN_CLASS=de.jochenskulj.taskmanager.Application
 
 java -classpath $CLASSPATH $MAIN_CLASS
